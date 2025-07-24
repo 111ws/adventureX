0a5fbe305ec5fd5fe3d4e804a92bbed6280a80ad
111ws <124578371+111ws@users.noreply.github.com>
2025-07-24 09:45:47 +0800

    Initial Commit

---
+++ adventureX/adventureXApp.swift
@@ -0,0 +1,17 @@
+//
+//  adventureXApp.swift
+//  adventureX
+//
+//  Created by 陆氏干饭王 on 24-07-2025.
+//
+
+import SwiftUI
+
+@main
+struct adventureXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
